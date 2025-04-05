d6b7b0cff2d77e4f8e8b40bcb352db1853d697ee
FernandoT8rres <al03060390@tecmilenio.mx>
2025-04-05 08:28:05 -0600

    CRUD

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,27 +1,21 @@
 <?php
-
 namespace Database\Seeders;
-use App\Models\User;
-use App\Models\Superhero;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;  // Esta línea importa la clase Hash
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-        $this -> call([
-            GenderSeeder::class,
-            UniverseSeeder::class,
-        ]);
-        Superhero::factory(100)->create();
+        // Verificar si el usuario con el correo ya existe antes de crear uno nuevo
+        if (!User::where('email', 'test@example.com')->exists()) {
+            User::create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+                'password' => Hash::make('password'), // Ahora Hash está disponible
+            ]);
+        }
     }
 }
